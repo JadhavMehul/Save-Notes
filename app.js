eaf29fb255b9d178aef3ea578edac56c75567b02
Manish Choudhary <manishchaudharyjee@gmail.com>
2021-02-12 23:13:55 +0530

    add something

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const passportLocalMongoose = require("passport-local-mongoose"); // use this fo
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
+const PORT = process.env.PORT;
 const app = express();
 
 var picture = String;
@@ -20,7 +21,7 @@ var name = String;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
     secret: "Our Little Secret",
@@ -35,59 +36,75 @@ app.use(passport.session());
 mongoose.connect(process.env.MONGO_DB_ADDRESS, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}).then(() => {
+    console.log(`MongoDB is connected Successfully`);
+}).catch((error) => {
+    console.log(error);
 });
 mongoose.set('useCreateIndex', true);
 
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String
+});
+const noteSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate)
 
 const User = new mongoose.model("User", userSchema)
+const Note = new mongoose.model("Note", noteSchema)
 
 passport.use(User.createStrategy());
 
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
-  
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
         done(err, user);
     });
 });
 
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientID: process.env.CLIENT_ID || 'dshksdhsdhhsdkj',
+    clientSecret: process.env.CLIENT_SECRET || 'sihsdhdh',
     callbackURL: "http://localhost:3000/auth/google/savenotes",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-    });
-    picture = profile._json.picture;
-    name = profile._json.name;
-    return (picture, name);
-  }
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+        picture = profile._json.picture;
+        name = profile._json.name;
+        return (picture, name);
+    }
 ));
 
 
 app.get("/", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.redirect("notes");
-    } else {
-        res.render("home");
-    }
+    // if (req.isAuthenticated()) {
+    //     res.redirect("notes");
+    // } else {
+    //     res.render("home");
+    // }
+
+    res.redirect("notes");
+
 });
 
 /*------------------ Google Api For Account ------------------*/
@@ -95,13 +112,13 @@ app.get("/auth/google",
     passport.authenticate("google", { scope: ["profile"] })
 );
 
-app.get("/auth/google/savenotes", 
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    
-    res.redirect("/notes");
-});
+app.get("/auth/google/savenotes",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+
+        res.redirect("/notes");
+    });
 
 app.get("/login", (req, res) => {
     res.render("login");
@@ -110,22 +127,47 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
     res.render("register");
 });
-
-app.get("/notes", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("notes", {profilePicture: picture, userName: name});
-    } else {
-        res.redirect("login");
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body
+    const user = await User.findOne({ email: username })
+    try {
+        if (user) {
+            res.send('User already registered')
+        } else {
+            const newUser = new User({
+                email: username,
+                password
+            })
+            await newUser.save();
+        }
+        res.status(200).redirect('/login');
+    } catch (error) {
+        console.log(`${error}`);
     }
 });
 
+app.get("/notes", (req, res) => {
+    // if (req.isAuthenticated()) {
+    //     res.render("notes", { profilePicture: picture, userName: name });
+    // } else {
+    //     res.redirect("login");
+    // }
+
+    res.render("notes", { profilePicture: picture, userName: name });
+
+});
+app.post('/notes', (req, res) => {
+    const { title, description } = req.body;
+    res.redirect('back')
+})
+
 /*------------------ Log out your Account ------------------*/
-app.get("/logout", function(req, res){
+app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
 });
 
 
-app.listen(3000, function() {
-    console.log("Server started at port 3000");
-});
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
